c211b29d4045339b5d0c93c9e332eb36c84e36b7
Adarsh Goswami <adarsh010120@gmail.com>
2022-03-27 04:32:19 +0530

    routing completed

--- frontend/src/layout/AppContainer.jsx
+++ frontend/src/layout/AppContainer.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { routes } from "../routes/routes";
 import Container from "./container/Container";
 import Header from "./header/Header";
+import Error404 from "../components/pages/404/Error404";
 
 const AppContainer = () => {
   return (
@@ -14,14 +15,13 @@ const AppContainer = () => {
       <Routes>
         {
           routes?.map(route => {
-            <Route
+            return <Route
               key={route?.id}
               path={route.path}
-              exact={route.exact} >
-              <Container>
+              exact={route.exact}
+              element={<Container className={route.className}>
                 <route.component />
-              </Container>
-            </Route>
+              </Container>} />
           })
         }
       </Routes>
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,26 +1,31 @@
+import Error404 from '../components/pages/404/Error404';
 import Home from '../components/pages/home/Home';
 
 export const routes= [
   {
     path: "/", 
+    className: "home",
     exact: true, 
     hideHeader: false, 
     component: Home, 
     id: 1, 
   },{
-    path: "/explore", 
+    path: "/explore",
+    className: "explore", 
     exact: true, 
     hideHeader: false, 
     component: <></>, 
     id: 2, 
   },{
     path: "/quizzes", 
+    className: "quizzes",
     exact: true, 
     hideHeader: false, 
     component: <></>, 
     id: 3, 
   },{
-    path: "/leaderboard", 
+    path: "/leaderboard",
+    className: "leaderboard", 
     exact: true, 
     hideHeader: false, 
     component: <></>, 
@@ -39,5 +44,12 @@ export const routes= [
     component: <></>, 
     id: 6, 
     protected: true,
+  },{
+    path: "*", 
+    exact: true, 
+    hideHeader: false, 
+    component: Error404, 
+    id: 7, 
+    protected: false,
   },
 ]
