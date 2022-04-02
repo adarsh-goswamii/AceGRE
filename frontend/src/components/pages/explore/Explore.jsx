25e649f38ed092224b953b3542682096318cda9d
Adarsh Goswami <adarsh010120@gmail.com>
2022-04-03 02:57:43 +0530

    pagination completed

--- frontend/src/components/pages/explore/Explore.jsx
+++ frontend/src/components/pages/explore/Explore.jsx
@@ -6,24 +6,34 @@ import RightPane from "../../../layout/rightDrawer/RightDrawer";
 import WordDetails from "../../widgets/wordDetails/WordDetails";
 import { useDispatch, useSelector } from "react-redux";
 import * as action from "../../../store/action/common";
+import Pagination from "../../widgets/pagination/Pagination";
+import { H3 } from "../../shared/typography/Typogrpahy";
 
 const Explore = () => {
   const rightDrawer = useSelector(state => state.common.rightDrawer);
   const dispatch = useDispatch();
   function handleRightPaneOpen(event) {
-    dispatch(action.showRightDrawer({open: true}));
+    dispatch(action.showRightDrawer({ open: true }));
   }
   function handleRightPaneClose() {
-    dispatch(action.showRightDrawer({open: false}));
+    dispatch(action.showRightDrawer({ open: false }));
   }
   return (
     <>
+      <H3 className="heading">Search Vocabulary Words</H3>
+      <div className="filters">
+
+      </div>
       <div className="word-grid-container">
         {
           words?.map(word => <WordCard {...word} onClick={handleRightPaneOpen} />)
         }
       </div>
       <div className="pagination">
+        <Pagination
+          limit={20}
+          paginationOptions={[20, 40, 60, 80, 100]}
+          totalPage={10} />
       </div>
       <RightPane
         open={rightDrawer?.open}
