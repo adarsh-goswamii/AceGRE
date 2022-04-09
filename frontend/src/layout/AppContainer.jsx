c7fd7970748dfaecae895b3e3a440ac55f194d49
Adarsh Goswami <adarsh010120@gmail.com>
2022-04-09 17:26:58 +0530

    login wip

--- frontend/src/layout/AppContainer.jsx
+++ frontend/src/layout/AppContainer.jsx
@@ -3,11 +3,27 @@ import { routes } from "../routes/routes";
 import Container from "./container/Container";
 import Header from "./header/Header";
 import Error404 from "../components/pages/404/Error404";
+import Login from "../components/pages/login/Login";
+import { useEffect, useState } from "react";
 
 const AppContainer = () => {
+  const [headerVisible, setHeaderVisible] = useState(false);
+
+  useEffect(() => {
+    console.log(renderHeader());
+    setHeaderVisible(renderHeader());
+  }, [window.location.pathname]);
+
+  function renderHeader() {
+    const pathname = window.location.pathname;
+    const currentRoute = routes.filter(route => route.path === pathname);
+    console.log(currentRoute, pathname);
+    return currentRoute && !currentRoute[0].hideHeader;
+  }
+
   return (
     <>
-      <Header />
+      {headerVisible ? <Header /> : <></>}
       {/* <Toaster />
       <RightDrawer />
       <Modal />
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,56 +1,65 @@
 import Error404 from '../components/pages/404/Error404';
 import Home from '../components/pages/home/Home';
 import Explore from '../components/pages/explore/Explore';
+import Login from '../components/pages/login/Login';
 
-export const routes= [
+export const routes = [
   {
-    path: "/", 
+    path: "/",
     className: "home",
-    exact: true, 
-    hideHeader: false, 
-    component: Home, 
-    id: 1, 
-  },{
+    exact: true,
+    hideHeader: false,
+    component: Home,
+    id: 1,
+  }, {
     path: "/explore",
-    className: "explore", 
-    exact: true, 
-    hideHeader: false, 
-    component: Explore, 
-    id: 2, 
-  },{
-    path: "/quizzes", 
+    className: "explore",
+    exact: true,
+    hideHeader: false,
+    component: Explore,
+    id: 2,
+  }, {
+    path: "/quizzes",
     className: "quizzes",
-    exact: true, 
-    hideHeader: false, 
-    component: <></>, 
-    id: 3, 
-  },{
+    exact: true,
+    hideHeader: false,
+    component: <></>,
+    id: 3,
+  }, {
     path: "/leaderboard",
-    className: "leaderboard", 
-    exact: true, 
-    hideHeader: false, 
-    component: null, 
-    id: 4, 
-  },{
-    path: "/user-profile", 
-    exact: true, 
-    hideHeader: false, 
-    component: <></>, 
-    id: 5, 
+    className: "leaderboard",
+    exact: true,
+    hideHeader: false,
+    component: null,
+    id: 4,
+  }, {
+    path: "/user-profile",
+    exact: true,
+    hideHeader: false,
+    component: <></>,
+    id: 5,
     protected: false,
-  },{
-    path: "/add-word", 
-    exact: true, 
-    hideHeader: false, 
-    component: <></>, 
-    id: 6, 
+  }, {
+    path: "/add-word",
+    exact: true,
+    hideHeader: false,
+    component: <></>,
+    id: 6,
     protected: true,
-  },{
-    path: "*", 
-    exact: true, 
-    hideHeader: false, 
-    component: Error404, 
-    id: 7, 
+  }, {
+    path: "/login",
+    exact: true,
+    hideHeader: true,
+    component: Login,
+    id: 8,
     protected: false,
-  },
+    className: "login-screen",
+  }, {
+    path: "*",
+    exact: true,
+    hideHeader: false,
+    component: Error404,
+    id: 7,
+    protected: false,
+  }
 ]
