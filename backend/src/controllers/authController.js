bab6181aaf0d806e2a7a2445ed982e5800e12be6
Adarsh Goswami <adarsh010120@gmail.com>
2022-04-10 04:55:03 +0530

    login and register endpoint completed

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,12 +1,52 @@
 const HttpError = require('../models/http-error');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/userSchema");
 
 const TOKEN_CREATION_KEY = "AGH78@NVS&98$ASLDM";
 const login = async (req, res, next) => {
-  console.log("Logging user in");
-
-
+  // TODO: handle token creation and renew.
+  try {
+    const { email, password, rememberMe } = req.body;
+    const userInfo = await User.findOne({email: email}).exec();
+    if(userInfo) {
+      const validPass = await bcrypt.compare(password, userInfo.password);
+      if(validPass) {
+        // user logged in generate a token for him.
+        res.status(200).json({
+          status: "success", 
+          data: {
+            email: email, 
+            token: "", 
+            message: "User successfully logged in"
+          }
+        });
+      } else {
+        // user exists but password is wrong
+        res.status(409).json({
+          status: "failure", 
+          data: {
+            email: email, 
+            token: "", 
+            message: "Email or password is wrong"
+          }
+        });
+      }
+    } else {
+      // no user with given email exists
+      res.status(404).json({
+        status: "failure", 
+        data: {
+          email: email, 
+          token: "", 
+          message: "No user with given email exists"
+        }
+      })
+    }
+  } catch (error) {
+    // internal error 
+    return next(new HttpError(error, 500));
+  }
 };
 
 const register = async (req, res, next) => {
@@ -17,9 +57,17 @@ const register = async (req, res, next) => {
       password: hashedPass,
       email: email,
     }, TOKEN_CREATION_KEY, { expiresIn: "1h" });
+
+    const newUser = new User({ email, password: hashedPass, admin: false });
+    await newUser.save();
+
     res.status(200).json({
       status: "success",
-      token: generatedToken,
+      data: {
+        token: generatedToken,
+        email: email,
+        admin: false,
+      }
     });
   } catch (error) {
     return next(new HttpError(error, 500));
