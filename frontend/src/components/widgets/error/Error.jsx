bddb1271affeef9380f95222db2536a94929cfdd
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2022-04-12 04:22:55 +0530

    Feat/login signup (#14)
    * login wip
    
    * authentication frontend completed
    
    * login api integrated
    
    * login signup api endpoints integrated

---
+++ frontend/src/components/widgets/error/Error.jsx
@@ -0,0 +1,23 @@
+import "./Error.scss";
+import { PropTypes } from "prop-types";
+import { ReactComponent as ErrorIcon } from "../../../assets/images/error.svg";
+
+const Error = ({
+  message
+}) => {
+  return (
+    <div className="error-container">
+      <ErrorIcon className="icon" />
+      <p className="error-message">{message}</p>
+    </div>
+  )
+};
+
+export default Error;
+
+Error.protoTypes = {
+  // necessary fields
+  message: PropTypes.string.isRequired, 
+  // optional fields
+  className: PropTypes.string
+}
--- frontend/src/components/widgets/login/SignUpForm.jsx
+++ frontend/src/components/widgets/login/SignUpForm.jsx
@@ -2,36 +2,140 @@ import { useState } from "react";
 import { FormControlLabel, Checkbox } from "@material-ui/core";
 import InputField from "../../shared/inputField/InputField";
 import Button from "../../shared/button/Button";
+import Error from "../error/Error";
+import { emailValidate, passwordStrengthCheck } from "../../../utility/validations";
+import { PASSWORD_DONT_MATCH, SOMETHING_WENT_WRONG } from "../../../constants/errorMessage.consts";
+import { register } from "../../../apis/auth";
+import { useNavigate } from "react-router-dom";
+
+const initInputField = {
+  value: "",
+  helperText: "",
+  error: false,
+  disabled: false
+};
+
 
 const SignUpForm = ({
   toggleForm
 }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
+  const navigate = useNavigate();
+  const [username, setUsername] = useState(new Object(initInputField));
+  const [password, setPassword] = useState(new Object(initInputField));
+  const [confirmPassword, setConfirmPassword] = useState(new Object(initInputField));
+  const [error, setError] = useState({ show: false, message: "" });
+
+  async function handleRegisterUser() {
+    try {
+      const response = await register({ email: username.value, password: password.value });
+      const { token: _token, email: _email, role } = response.data;
+      localStorage.setItem("token", _token);
+      localStorage.setItem("email", _email);
+      localStorage.setItem("role", role);
+      navigate("/");
+    } catch (error) {
+      let errorMessage = error?.response?.data?.data?.message;
+      errorMessage= errorMessage ? errorMessage : SOMETHING_WENT_WRONG;
+      setError({ show: true, message: errorMessage });
+    }
+    setPassword(new Object(initInputField));
+    setConfirmPassword(new Object(initInputField));
+  }
+
+  function handleEmailChange(value) {
+    const helperText = username.error ? ( username.error ? emailValidate(value) : "") : "";
+    setUsername(prev => Object.assign({}, prev, {
+      helperText,
+      value,
+      error: Boolean(helperText),
+    }));
+  };
+
+  function handlePasswordChange(value) {
+    const helperText = password.error ? (password.value ? passwordStrengthCheck(value) : "") : "";
+    setPassword(prev => Object.assign({}, prev, {
+      helperText,
+      value,
+      error: Boolean(helperText)
+    }));
+
+    if(confirmPassword.value) {
+      const helperText = value === confirmPassword.value ? "" : PASSWORD_DONT_MATCH; 
+      setConfirmPassword(prev => Object.assign({}, prev, {
+        helperText, 
+        error: Boolean(helperText)
+      }))
+    }
+  };
+
+  function handleConfirmPasswordChange(value) {
+    const helperText = confirmPassword.error ? (password.value === value ? "" : PASSWORD_DONT_MATCH) : "";
+    setConfirmPassword(prev => Object.assign({}, prev, {
+      helperText,
+      value,
+      error: Boolean(helperText)
+    }));
+  };
+
+  function handleOnBlurUsername() {
+    const temp = emailValidate(username.value);
+    setUsername(prev => {
+      return Object.assign({}, prev, {
+        helperText: temp,
+        error: Boolean(temp),
+      });
+    });
+  };
+
+  function handleOnBlurPassword() {
+    const temp = passwordStrengthCheck(password.value);
+    console.log(temp);
+    setPassword(prev => Object.assign({}, prev, {
+      helperText: temp,
+      error: Boolean(temp),
+    }));
+  };
+
+  function handleOnBlurConfirmPassword() {
+    const temp = password.value === confirmPassword.value ? "" : PASSWORD_DONT_MATCH;
+    setConfirmPassword(prev => Object.assign({}, prev, {
+      helperText: temp,
+      error: Boolean(temp),
+    }));
+  };
+
+  const registerDisabled = !username.value || !password.value || username.error || password.error || confirmPassword.error;
 
   return (
     <>
       <div className="heading">REGISTER</div>
       <InputField
-        value={username}
+        value={username.value}
         className="input-field necessary"
         type="text"
-        label="Username / Email"
-        onChange={setUsername} />
+        error={username.error}
+        helperText={username.helperText}
+        label="Email Address"
+        onBlur={handleOnBlurUsername}
+        onChange={handleEmailChange} />
       <InputField
-        value={password}
+        value={password.value}
         className="input-field necessary"
+        error={password.error}
+        helperText={password.helperText}
         type="password"
         label="Password"
-        onChange={setPassword} />
+        onBlur={handleOnBlurPassword}
+        onChange={handlePasswordChange} />
       <InputField
-        value={confirmPassword}
+        value={confirmPassword.value}
         className="input-field necessary"
+        error={confirmPassword.error}
+        onBlur={handleOnBlurConfirmPassword}
+        helperText={confirmPassword.helperText}
         type="password"
         label="Confirm Password"
-        onChange={setConfirmPassword} />
+        onChange={handleConfirmPasswordChange} />
 
       <p className="helper-text-login">
         {"Already have an account ? "}
@@ -48,9 +152,12 @@ const SignUpForm = ({
       <Button
         variant="contained"
         className="btn"
-        onClick={() => {}}>
+        disabled={registerDisabled}
+        onClick={handleRegisterUser}>
         Register
       </Button>
+
+      {error.show && <Error message={error.message} />}
     </>
   )
 }
