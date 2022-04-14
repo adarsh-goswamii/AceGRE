92bcf2fff5d2e8dad23ae1418881674e46aa5b96
Adarsh Goswami <adarsh010120@gmail.com>
2022-04-14 23:57:02 +0530

    get words using token added

--- backend/src/controllers/wordsController.js
+++ backend/src/controllers/wordsController.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const Word = require('../models/wordSchema');
+const UserData = require('../models/userDataSchema');
 const HttpError = require('../models/http-error');
+const jwt = require("jsonwebtoken")
 
 const addWord = async (req, res, next) => {
     // add validations.
@@ -33,25 +36,52 @@ const addWord = async (req, res, next) => {
 
 const getWords = async (req, res, next) => {
     try {
+        let access_token = req.headers["authorization"];
+        if (access_token) access_token = req.headers["authorization"].split(" ")[1];
+
+        console.log(access_token);
         let { filter, pagination: { size, page_no } } = req.body;
         page_no--;
         const wordsCount = await Word.count();
-        const data = await Word.find({}, null, { skip: size * page_no, limit: size });
-        res.status(200).json({
-            status: "success",
-            data: data,
-            pagination: {
-                size,
-                page_no: page_no + 1,
-                total_pages: Number(Math.ceil(wordsCount / size))
-            }
-        })
+        let wordList = await Word.find({}, null, { skip: size * page_no, limit: size }).lean().exec();
+        if (access_token) {
+            // user is logged in
+            let userData;
+            jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, async (err, data) => {
+                if (err) return res.status(403).json("Unauthorized Access");
 
+                const { id } = data;
+                userData = await UserData.findOne({ id }).lean().exec();
+                wordList = wordList.map(word => {
+                    const status = userData.word_status[word._id.toString()] || null;
+                    return Object.assign({}, word, { status });
+                });
+    
+                res.status(200).json({
+                    status: "success",
+                    data: wordList,
+                    pagination: {
+                        size,
+                        page_no: page_no + 1,
+                        total_pages: Number(Math.ceil(wordsCount / size))
+                    }
+                });
+            });
+        } else {
+            res.status(200).json({
+                status: "success",
+                data: wordList,
+                pagination: {
+                    size,
+                    page_no: page_no + 1,
+                    total_pages: Number(Math.ceil(wordsCount / size))
+                }
+            });
+        }
     } catch (error) {
         return next(new HttpError(error, 500));
     }
 };
-
 
 module.exports = {
     addWord,
