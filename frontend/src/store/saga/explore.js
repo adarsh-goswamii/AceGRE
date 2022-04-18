afe43d8274bd5c90ffdefa0154d610b39882f8c1
Adarsh Goswami <adarsh010120@gmail.com>
2022-04-18 09:56:08 +0530

    loader added

--- frontend/src/store/saga/explore.js
+++ frontend/src/store/saga/explore.js
@@ -5,12 +5,24 @@ import * as api from "../../apis/explore";
 function* getWordList(action) {
   try {
     const payload = action.payload;
+    yield put({
+      type: actionType.CHANGE_GLOBAL_LOADER_VISIBILITY, 
+      payload: true
+    });
     const results = yield call(api.getWordList, payload);
     yield put({
       type: actionType.GET_WORD_LIST_SUCCESS, 
       payload: results
     });
+    yield put({
+      type: actionType.CHANGE_GLOBAL_LOADER_VISIBILITY, 
+      payload: false 
+    });
   } catch(error) {
+    yield put({
+      type: actionType.CHANGE_GLOBAL_LOADER_VISIBILITY, 
+      payload: false
+    });
     yield put({
       type: actionType.GET_WORD_LIST_FAILURE, 
       payload: error.response.data
