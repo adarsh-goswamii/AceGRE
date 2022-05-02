6bf1a52e90cf95e9902e1ea2f4a5f3e0c220136e
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2022-05-03 05:29:22 +0530

    Deployment updates. (#44)
    * refresh token implemented
    
    * refresh token completed
    
    * deployment changes
    
    * baseurl updated for development env

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,10 +36,14 @@ app.use((error, req, res, next) => {
     res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+}
+
 async function connect() {
     try {
         let db = await mongoose.connect(process.env.MONGO_URL);
-        app.listen(process.env.SERVER_PORT);
+        app.listen(process.env.PORT || 5000);
         console.log('CONNECTION SUCCESSFULL'); 
     } catch(error) {
         console.log('Could not connect to the database try again later', error);
