02ec4741b2f5e6cde814776f601bc9214859b2d4
Adarsh Goswami <adarsh010120@gmail.com>
2022-05-06 02:41:59 +0530

    no results lottie added

--- frontend/src/components/pages/explore/Explore.jsx
+++ frontend/src/components/pages/explore/Explore.jsx
@@ -14,6 +14,8 @@ import { ReactComponent as SearchIcon } from "../../../assets/images/search.svg"
 import { getWordList, updatePagination } from "../../../store/action/explore";
 import { plainToClass } from "class-transformer";
 import { Word } from "../../../model/Word";
+import Lottie from "lottie-react";
+import secure from "../../../assets/lottie/noResults.json";
 
 const Explore = () => {
   const [filterStatus, setFilterStatus] = useState(wordMenu[0]);
@@ -166,11 +168,20 @@ const Explore = () => {
           })}
         </Select>
       </div>
-      <div className="word-grid-container">
-        {words?.map((word) => (
-          <WordCard word={word} onClick={handleRightPaneOpen} />
-        ))}
-      </div>
+      {
+        words && words.length > 0 ?
+          <div className="word-grid-container">
+            {words?.map((word) => (
+              <WordCard word={word} onClick={handleRightPaneOpen} />
+            ))
+            }
+          </div>
+          :
+          (filter.search || filter.status) ?
+            <Lottie animationData={secure} className="lottie" />
+            :
+            <></>
+      }
       <div className="pagination">
         <Pagination
           page={pagination.page_no}
