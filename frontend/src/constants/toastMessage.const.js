2d3fdf1ce063fd88bf6bcf62fe5ecd62b49b06ea
Adarsh Goswami <63430847+Adarsh-Goswamii@users.noreply.github.com>
2022-05-06 03:50:13 +0530

    Bug - update status for not logged in user resolved. (#58)
    * result correction check fixed
    
    * routing fixed in unauthorized access
    
    * avatar menu name changed
    
    * explore filter resolved
    
    * filters reset when page initially loaded fixed
    
    * no results lottie added
    
    * quiz stepper fixed
    
    * update status for not logged in user resolved

--- frontend/src/constants/toastMessage.const.js
+++ frontend/src/constants/toastMessage.const.js
@@ -2,4 +2,5 @@ export const QUIZ_SOLUTION_RECORDED_SUCCESSFULLY = "Answer recorded successfully
 export const WORD_STATUS_UPDATED_SUCCESSFULLY = "Word status updated successfully!";
 
 export const SOMETHING_WENT_WRONG = "Something went wrong!";
+export const LOGIN_TO_ACCESS_THIS_FEATURE = "Login to use this feature";
 
