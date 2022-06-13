d0430ef267cdec18d7afe565229e8bdaf1cafa9b
Adarsh Goswami <adarsh010120@gmail.com>
2022-06-13 15:38:12 +0530

    add word form completed except part of speech

--- frontend/src/components/pages/addWord/AddWord.jsx
+++ frontend/src/components/pages/addWord/AddWord.jsx
@@ -1,15 +1,47 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import DynamicInputs from "../../widgets/dynamicInputs/DynamicInputs";
 import InputField from "../../shared/inputField/InputField";
 import Button from "../../shared/button/Button";
+import {addWord} from "../../../store/action/addWord";
+import {useDispatch, useSelector} from "react-redux";
 import styles from "./addWord.module.scss";
 
 const AddWord = ({ }) => {
+  const dispatch = useDispatch();
   const [word, setWord] = useState("");
   const [funFact, setFunFact] = useState("");
   const [meanings, setMeanings] = useState([""]);
   const [sentences, setSentences] = useState([""]);
   const [mneumonics, setMneumonics] = useState([""]);
+
+  const wordAdded = useSelector(state => state.addWord.wordAddedSuccessfully);
+
+  useEffect(() => {
+    if(wordAdded) {
+      handleResetClick();
+    }
+  }, [wordAdded])
+
+  function handleAddWordClick () {
+    const payload = {
+      title: word, 
+      part_of_speech: "verb", 
+      fun_fact: funFact, 
+      meanings: meanings.filter(data => Boolean(data.trim())).map(data => ({meaning: data})), 
+      sentences: sentences.filter(data => Boolean(data.trim())).map(data => ({sentence: data})),
+      mneumonics: mneumonics.filter(data => Boolean(data.trim())).map(data => ({mneumonic: data})),
+    };
+    dispatch(addWord(payload));
+  };
+
+  function handleResetClick() {
+    setWord("");
+    setFunFact("");
+    setMeanings([""]);
+    setSentences([""]);
+    setMneumonics([""]);
+  }
+
   return (
     <div className={styles["word-form"]}>
       <p className={styles["word-form__heading"]}>
@@ -52,7 +84,7 @@ const AddWord = ({ }) => {
 
       <div className={styles["word-form__action-btns"]}>
         <Button variant="outlined">Reset</Button>
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" onClick={handleAddWordClick}>Submit</Button>
       </div>
     </div>
   )
--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -3,12 +3,14 @@ import common from "./common";
 import auth from "./auth";
 import explore from "./explore";
 import quiz from "./quiz";
+import addWord from "./addWord";
 
 const rootReducer = combineReducers({
   common,
   auth,
   explore,
   quiz,
+  addWord
 });
 
 export default rootReducer;
