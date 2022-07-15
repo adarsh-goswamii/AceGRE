aa981fe603077fb3b7dcfc7a2f34461878f6f11d
Adarsh Goswami <adarsh010120@gmail.com>
2022-07-15 05:47:04 +0530

    404 page recreated

--- frontend/src/components/pages/404/Error404.jsx
+++ frontend/src/components/pages/404/Error404.jsx
@@ -1,7 +1,49 @@
-import { H1 } from "../../shared/typography/Typogrpahy";
+import { H1, H3, H4 } from "../../shared/typography/Typogrpahy";
+import error404 from "../../../assets/lottie/404.json";
+import { HELPER_TEXT, OOPS, PAGE_NOT_FOUND } from "./constant";
+import Lottie from "lottie-react";
+import Chip from "../../shared/chip/Chip";
+import { useHistory } from "react-router-dom";
+import styles from "./error404.module.scss";
 
 const Error404 = () => {
-  return <H1>404 Page not found!!</H1>;
+  const history = useHistory();
+
+  return (
+    <div className={styles["error404"]}>
+      <Lottie animationData={error404} className={styles["error404__animation"]} />
+      <div className={styles["error404__content"]}>
+        <div>
+          <H1 className={styles["error404__heading"]}>{OOPS}</H1>
+          <H3>{PAGE_NOT_FOUND}</H3>
+        </div>
+        <div className={styles["error404__chips"]}>
+          <H4 className={styles["error404__helper-text"]}>{HELPER_TEXT}</H4>
+          <Chip
+            label={"Explore new words"}
+            color={"primary"}
+            variant={"outlined"}
+            className={styles["error404__chip"]}
+            onClick={() => history.push("/explore")}
+            clickable={true} />
+          <Chip
+            label={"Take a quiz"}
+            color={"primary"}
+            variant={"outlined"}
+            className={styles["error404__chip"]}
+            onClick={() => history.push("/quiz")}
+            clickable={true} />
+          <Chip
+            label={"Navigate to home"}
+            color={"primary"}
+            variant={"outlined"}
+            className={styles["error404__chip"]}
+            onClick={() => history.push("/")}
+            clickable={true} />
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Error404;
